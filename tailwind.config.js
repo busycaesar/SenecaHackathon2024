054235b03b47a01e8ebca6de70c4a3161f7c304a
Yanice <yyoyosze@gmail.com>
2024-01-06 18:33:28 -0500

    About Us updated

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    important: true,
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
