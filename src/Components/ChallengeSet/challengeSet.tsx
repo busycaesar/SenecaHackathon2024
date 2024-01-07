72f367e61a318866f504fd17d337579aae3d5ef4
Dev Shah <busycaesar@gmail.com>
2024-01-07 11:14:01 -0500

    Developed challenge set component!

--- src/Components/ChallengeSet/challengeSet.tsx
+++ src/Components/ChallengeSet/challengeSet.tsx
@@ -1,9 +1,21 @@
 import "./challengeSet.css";
 
-export default function ChallengeSet(props: { content: string }) {
+export default function ChallengeSet(props: {
+  title: string;
+  subtitle: string;
+  content: string;
+}) {
   return (
     <>
-      <div className="challenge-set">{props.content}</div>
+      <div className="challenge-set">
+        <div className="title">
+          <h2>
+            <strong>{props.title}</strong>
+          </h2>
+          <h3>{props.subtitle}</h3>
+        </div>
+        <p>{props.content}</p>
+      </div>
     </>
   );
 }
--- src/Pages/challengeSets.tsx
+++ src/Pages/challengeSets.tsx
@@ -7,7 +7,11 @@ export default function ChallengeSets() {
       <h1>
         <strong>Challenge Sets</strong>
       </h1>
-      <ChallengeSet content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse doloribus, fuga laudantium, vel laborum repellendus nemo dicta ullam ut voluptatibus consectetur. Quasi, dolorem. Qui vero porro architecto quaerat ipsam neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolorem incidunt? Tenetur iure velit maxime autem sunt, veritatis quam sequi. Tempore incidunt earum vero minus quisquam voluptates cupiditate ducimus provident?" />
+      <ChallengeSet
+        title="Lorem ipsum dolor sit amet consectetur"
+        subtitle="Lorem ipsum dolor sit amet consectetur"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse doloribus, fuga laudantium, vel laborum repellendus nemo dicta ullam ut voluptatibus consectetur. Quasi, dolorem. Qui vero porro architecto quaerat ipsam neque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, dolorem incidunt? Tenetur iure velit maxime autem sunt, veritatis quam sequi. Tempore incidunt earum vero minus quisquam voluptates cupiditate ducimus provident?"
+      />
     </>
   );
 }
