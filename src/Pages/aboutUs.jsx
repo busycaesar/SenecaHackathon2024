c7a58548ef8f8f3f9eb29ecb716cfb6142d793b0
Santosharawn7 <santosharawn7@gmail.com>
2024-01-14 01:19:17 -0500

    color fixed

--- src/Pages/aboutUs.jsx
+++ src/Pages/aboutUs.jsx
@@ -1,70 +1,67 @@
 export default function AboutUs() {
   return (
     <>
-    <div>
+      <div>
         <h1 class="text-center text-3xl font-bold mb-3">
           About the Hackathon
         </h1>
         <h3 class="text-center mb-5">March 16, 2024</h3>
-    </div>
-    <div class="max-w-lg w-full m-5 lg:max-w-full lg:flex ">
-  <div class="h-48 flex-none rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"  title="Woman holding a mug">
-  </div>
-  <div class="bg-my-red mb-5 border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-    <div class="mb-8">
-      <div class="text-white font-bold text-center text-xl mb-2">What is a Hackathon?</div>
-      <p class="text-white text-base text-justify m-5">The hackathon is a student-focused innovation competition that provides a unique experiential learning experience. It encourages and develops creative thinking, entrepreneurship, and problem-solving skills by bringing together students from diverse backgrounds to work together on addressing real-world challenges. The teams receive support from subject-specific mentors and technical specialists and then showcase their solutions in a fun and lively environment to be adjudicated by a panel of experts consisting of investors, partners, school representatives, and domain specialists. The winning teams will have the opportunity to receive prizes and be considered for applied research project investment opportunities. </p>
-    </div>
-    <div class="flex items-center">
-    </div>
-  </div>
-</div>
+      </div>
+      <div class="max-w-lg w-full m-5 lg:max-w-full lg:flex ">
+        <div class="card mb-5 border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+          <div class="mb-8">
+            <div class="text-white font-bold text-center text-xl mb-2">What is a Hackathon?</div>
+            <p class="text-white text-base text-justify m-5">The hackathon is a student-focused innovation competition that provides a unique experiential learning experience. It encourages and develops creative thinking, entrepreneurship, and problem-solving skills by bringing together students from diverse backgrounds to work together on addressing real-world challenges. The teams receive support from subject-specific mentors and technical specialists and then showcase their solutions in a fun and lively environment to be adjudicated by a panel of experts consisting of investors, partners, school representatives, and domain specialists. The winning teams will have the opportunity to receive prizes and be considered for applied research project investment opportunities. </p>
+          </div>
+          <div class="flex items-center">
+          </div>
+        </div>
+      </div>
 
-<div class="max-w-lg w-full m-5 lg:max-w-full lg:flex ">
-  <div class="h-48 flex-none  rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"  title="Woman holding a mug">
-  </div>
-  <div class="bg-my-red border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-    <div class="">
-      <div class="text-white font-bold text-center text-xl mb-2">How it all started</div>
-      <p class="text-white text-base text-justify m-5">Seneca Hackathon began in July 2020 as a response to the COVID-19 pandemic – gathering passionate students to channel their talents and skills into crafting solutions that will address the challenges brought forth by the pandemic. The success of the first Hackathon paved the way for it to evolve into an annual contest with the goal to tackle the most relevant and pressing issues of our society.  </p>
-    </div>
-    <div class="flex items-center">
-    </div>
-  </div>
-</div>
+      <div class="max-w-lg w-full m-5 lg:max-w-full lg:flex ">
+        
+        <div class="card border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+          <div class="">
+            <div class="text-white font-bold text-center text-xl mb-2">How it all started</div>
+            <p class="text-white text-base text-justify m-5">Seneca Hackathon began in July 2020 as a response to the COVID-19 pandemic – gathering passionate students to channel their talents and skills into crafting solutions that will address the challenges brought forth by the pandemic. The success of the first Hackathon paved the way for it to evolve into an annual contest with the goal to tackle the most relevant and pressing issues of our society.  </p>
+          </div>
+          <div class="flex items-center">
+          </div>
+        </div>
+      </div>
       <div>
         <section class="py-5 m-5">
-  <div class="container font-sans px-4 mx-auto bg-gray-200">
-    <div class="max-w-2xl mx-auto">
-      <h2 class="mb-5 pt-5 text-3xl text-red-500 font-semibold font-heading">Who we are</h2>
-      <h3 class="mb-3 text-xl font-medium underline">The organizing team</h3>
-      <p class="mb-6 text-xl  text-justify text-black">Seneca Hackathon is comprised of dedicated organizing committee of volunteers – skilled professionals and enthusiastic students who lead the way to steer and integrate our mission and vision, ensuring seamless execution of the entire Hackathon event.  </p>
-      <h3 class="mb-3 text-xl font-medium underline">The participants </h3>
-      <p class="mb-6 text-xl  text-justify text-black">Our participants are students and alumni of Seneca and its partner institutions with wide-ranging interests and diverse backgrounds united and driven by shared passion for innovation.  </p>
-      <h3 class="mb-3 text-xl font-medium underline">The sponsors  </h3>
-      <p class="mb-10 text-xl  text-justify text-black">Our sponsors and partners help us shape our challenge sets according to the theme of the hackathon and extend their resources and expertise in support to the overall success of the event.  </p>
-      <h2 class="mb-3 text-2xl text-red-500 italic font-normal font-heading">Our vision </h2>
-      <p class="mb-10 text-xl  text-justify text-black">Empowering innovation, fostering collaboration.</p>
-      <h2 class="mb-3 text-2xl text-red-500 italic font-normal font-heading">Our mission </h2>
-      <p class="mb-3 text-xl  text-justify text-black">Uniting talents with shared passion </p>
-      <ul role="list" class="mb-6 marker:text-red-500 list-disc pl-5 space-y-3 text-black text-justify">
-        <li>Create innovative and impactful hackathon experience (Commitment to Participants) </li>
-        <li>Set new standards for excellence in organizing hackathons (Commitment to Hackathon Organizing Committee) </li>
-        <li>Foster a community of dedicated to continuous learning and growth (Commitment to Staff Member) </li>
-      </ul>
-      <h2 class="mb-3 text-2xl text-red-500 italic font-normal font-heading">Our Manifesto  </h2>
-      <ul role="list" class="pb-5 marker:text-red-500 list-disc pl-5 space-y-3 text-black text-justify">
-        <li>Diverse – Diversity in team composition greatly affects outcome. </li>
-        <li>Inclusive – No program or level of experience is left out. </li>
-        <li>Fun – Incredible energy level, creativity, networking, teamwork, prices, SWAG  </li>
-        <li>By Students – For Students – Top experiential learning opportunities  </li>
-        <li>Market Value Accelerator – Resume builder, interview content, micro-credential </li>
-      </ul>
-    </div>
-  </div>
-</section>
+          <div class="container font-sans px-4 mx-auto bg-gray-200">
+            <div class="max-w-2xl mx-auto">
+              <h2 class="mb-5 pt-5 text-3xl text-red-500 font-semibold font-heading">Who we are</h2>
+              <h3 class="mb-3 text-xl font-medium underline">The organizing team</h3>
+              <p class="mb-6 text-xl  text-justify text-black">Seneca Hackathon is comprised of dedicated organizing committee of volunteers – skilled professionals and enthusiastic students who lead the way to steer and integrate our mission and vision, ensuring seamless execution of the entire Hackathon event.  </p>
+              <h3 class="mb-3 text-xl font-medium underline">The participants </h3>
+              <p class="mb-6 text-xl  text-justify text-black">Our participants are students and alumni of Seneca and its partner institutions with wide-ranging interests and diverse backgrounds united and driven by shared passion for innovation.  </p>
+              <h3 class="mb-3 text-xl font-medium underline">The sponsors  </h3>
+              <p class="mb-10 text-xl  text-justify text-black">Our sponsors and partners help us shape our challenge sets according to the theme of the hackathon and extend their resources and expertise in support to the overall success of the event.  </p>
+              <h2 class="mb-3 text-2xl text-red-500 italic font-normal font-heading">Our vision </h2>
+              <p class="mb-10 text-xl  text-justify text-black">Empowering innovation, fostering collaboration.</p>
+              <h2 class="mb-3 text-2xl text-red-500 italic font-normal font-heading">Our mission </h2>
+              <p class="mb-3 text-xl  text-justify text-black">Uniting talents with shared passion </p>
+              <ul role="list" class="mb-6 marker:text-red-500 list-disc pl-5 space-y-3 text-black text-justify">
+                <li>Create innovative and impactful hackathon experience (Commitment to Participants) </li>
+                <li>Set new standards for excellence in organizing hackathons (Commitment to Hackathon Organizing Committee) </li>
+                <li>Foster a community of dedicated to continuous learning and growth (Commitment to Staff Member) </li>
+              </ul>
+              <h2 class="mb-3 text-2xl text-red-500 italic font-normal font-heading">Our Manifesto  </h2>
+              <ul role="list" class="pb-5 marker:text-red-500 list-disc pl-5 space-y-3 text-black text-justify">
+                <li>Diverse – Diversity in team composition greatly affects outcome. </li>
+                <li>Inclusive – No program or level of experience is left out. </li>
+                <li>Fun – Incredible energy level, creativity, networking, teamwork, prices, SWAG  </li>
+                <li>By Students – For Students – Top experiential learning opportunities  </li>
+                <li>Market Value Accelerator – Resume builder, interview content, micro-credential </li>
+              </ul>
+            </div>
+          </div>
+        </section>
 
-     
+
       </div>
     </>
   );
