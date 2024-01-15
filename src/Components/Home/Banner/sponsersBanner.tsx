534a1da3e467ae6c37ec7d1d437aa4452738ce87
Dev Shah <busycaesar@gmail.com>
2024-01-15 16:58:18 -0500

    Added alignment to the Sponser's logo!

--- src/Components/Home/Banner/sponsersBanner.tsx
+++ src/Components/Home/Banner/sponsersBanner.tsx
@@ -1,6 +1,7 @@
 import { Row, Col } from "react-bootstrap";
 import SenecaAppliedResearch from "../../../svgs/seneca-applied-research.png";
 import SenecaPolytechnic from "../../../svgs/seneca-polytechnic.png";
+import "./banner.css";
 
 export default function SponsersBanner() {
   return (
@@ -8,19 +9,15 @@ export default function SponsersBanner() {
       <h2 style={{ marginTop: "1em" }}>
         <strong>Our Sponsers</strong>
       </h2>
-      <Row style={{ textAlign: "center" }}>
-        <Col sm={6}>
+      <Row>
+        <Col className="center-hv" sm={6}>
           <img
             src={SenecaAppliedResearch}
             width={500}
             alt="Seneca Innovation"
           />
         </Col>
-        {/* <Col sm={2}>Sponser Logo</Col>
-      <Col sm={2}>Sponser Logo</Col>
-      <Col sm={2}>Sponser Logo</Col>
-    <Col sm={2}>Sponser Logo</Col> */}
-        <Col sm={6}>
+        <Col className="center-hv" sm={6}>
           <img src={SenecaPolytechnic} width={200} alt="Seneca Innovation" />
         </Col>
       </Row>
