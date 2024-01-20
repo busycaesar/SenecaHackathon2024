c16841d329cadf92138ed7d3fe818c94d40db3f1
Nonthachai Plodthong <nonthachaiplodthong@Nonthachais-MacBook-Pro.local>
2024-01-20 04:06:41 -0500

    fix tailwind config

--- src/Components/SocialMedia/socialMedia.tsx
+++ src/Components/SocialMedia/socialMedia.tsx
@@ -1,4 +1,3 @@
-import { Row, Col } from "react-bootstrap";
 import Instagram from "../../svgs/SocialMedia/instagram.png";
 import Facebook from "../../svgs/SocialMedia/facebook.png";
 import LinkedIn from "../../svgs/SocialMedia/linkedin.png";
@@ -7,69 +6,67 @@ import Tiktok from "../../svgs/SocialMedia/tiktok.png";
 
 export default function SocialMediaIcons() {
   return (
-    <Row>
-      <Col>
-        <a
-          href="https://www.instagram.com/senecahackathon/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={Instagram}
-            style={{ margin: "0.05em 0.25em" }}
-            alt="Link to Instagram"
-            width={20}
-          />
-        </a>
-        <a
-          href="https://www.facebook.com/SenecaHackathon2022"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={Facebook}
-            style={{ margin: "0.05em 0.25em" }}
-            alt="Link to Facebook"
-            width={20}
-          />
-        </a>
-        <a
-          href="https://www.linkedin.com/company/seneca-hackathon/mycompany/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={LinkedIn}
-            style={{ margin: "0.05em 0.25em" }}
-            alt="Link to LinkedIn"
-            width={20}
-          />
-        </a>
-        <a
-          href="https://twitter.com/SenecaHackathon"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={Twitter}
-            style={{ margin: "0.05em 0.25em" }}
-            alt="Link to Twitter"
-            width={20}
-          />
-        </a>
-        <a
-          href="https://www.tiktok.com/@senecahackathon"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={Tiktok}
-            style={{ margin: "0.05em 0.25em" }}
-            alt="Link to Tiktok"
-            width={20}
-          />
-        </a>
-      </Col>
-    </Row>
+    <div className="flex flex-row gap-x-2">
+      <a
+        href="https://www.instagram.com/senecahackathon/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={Instagram}
+          style={{ margin: "0.05em 0.25em" }}
+          alt="Link to Instagram"
+          width={20}
+        />
+      </a>
+      <a
+        href="https://www.facebook.com/SenecaHackathon2022"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={Facebook}
+          style={{ margin: "0.05em 0.25em" }}
+          alt="Link to Facebook"
+          width={20}
+        />
+      </a>
+      <a
+        href="https://www.linkedin.com/company/seneca-hackathon/mycompany/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={LinkedIn}
+          style={{ margin: "0.05em 0.25em" }}
+          alt="Link to LinkedIn"
+          width={20}
+        />
+      </a>
+      <a
+        href="https://twitter.com/SenecaHackathon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={Twitter}
+          style={{ margin: "0.05em 0.25em" }}
+          alt="Link to Twitter"
+          width={20}
+        />
+      </a>
+      <a
+        href="https://www.tiktok.com/@senecahackathon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={Tiktok}
+          style={{ margin: "0.05em 0.25em" }}
+          alt="Link to Tiktok"
+          width={20}
+        />
+      </a>
+    </div>
   );
 }
