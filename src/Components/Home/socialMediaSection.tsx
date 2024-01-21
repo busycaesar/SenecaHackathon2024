dfe40b0676dd02ef1e5a61b51789f8b51d4ff684
Dev Shah <busycaesar@gmail.com>
2024-01-21 18:45:05 -0500

    Updated title connect with us!

--- src/Components/Home/socialMediaSection.tsx
+++ src/Components/Home/socialMediaSection.tsx
@@ -5,7 +5,7 @@ export default function SocialMediaSection() {
   return (
     <>
       <h2>
-        <strong>Our Social Media</strong>
+        <strong>Connect with us</strong>
       </h2>
       <p className="w-full sm:w-10/12 lg:w-1/2">
         Follow us on our social media platforms for all the latest updates on
