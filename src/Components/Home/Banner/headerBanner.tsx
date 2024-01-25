49684f89593e70e7b3b33fecf771c32a3b0af670
Dhruvik-Sakariya <146869129+Dhruvik-Sakariya@users.noreply.github.com>
2024-01-25 16:49:57 +0530

    Update headerBanner.tsx

--- src/Components/Home/Banner/headerBanner.tsx
+++ src/Components/Home/Banner/headerBanner.tsx
@@ -1,26 +1,31 @@
+import { Row, Col } from "react-bootstrap";
 import { Sponsers } from "../../../Data/Schema/sponsers";
+import { Partners } from "../../../Data/Schema/partners";
 import SampleImage from "../../../svgs/banner.jpg";
 import Timer from "../../Timer/Timer";
 import "../about.css";
 import SponsersBanner from "./sponsersBanner";
+import {
+  SponsersData,
+  PartnersData
+} from "../../../Data/JSONData/";
 
 
-export default function HeaderBanner(props: { sponsers: Sponsers[] }) {
+export default function HeaderBanner(props: {
+  partners: Partners[]; sponsers: Sponsers[] 
+}) {
   return (
-    
-      
-      <div>
-
-      <section className="md:max-w-full ">
-        <div className="container md:max-w-full">
-          <div className="px-8 py-20 overflow-hidden rounded-xl background-banner" >
+      <div className="">
+      <section className="md:max-w-full">
+        <div className="md:max-w-full">
+          <div className="px-8 md:py-20 py-16 overflow-hidden rounded-xl background-banner" >
             <div className="md:max-w-6xl text-center mx-auto">
               <h1 className="font-heading mb-6 md:my-10 text-4xl md:text-5xl lg:text-6xl text-white font-black tracking-tight"><span>Housing Hackathon </span></h1>
-              <p className="mb-8 text-lg text-white ">Elevate your skills, connect with industry experts, and transform
+              <p className=" mb-11 text-base text-justify md:text-center md:text-lg text-white ">Elevate your skills, connect with industry experts, and transform
                     your ideas into reality. <br></br> Join the hackathon for a chance to win
                     amazing prizes and contribute to shaping the future of the housing
                     industry.</p>
-              <div className="max-w-lg mx-auto">
+              <div className="md:max-w-lg md:mx-auto ">
               <div className="button">
                   
                   <a
@@ -31,10 +36,10 @@ export default function HeaderBanner(props: { sponsers: Sponsers[] }) {
                     <button className="font-bold">Click Here to Register</button>
                   </a>
                 </div>
-                <div>
+                <div className="">
                   <Timer />
                 </div>
-                <p className="m-8 text-lg text-white">Deadline to register is on February 26, 2024</p>
+                <p className="m-4 md:text-lg text-base text-white">Deadline to register is on February 26, 2024</p>
               </div>
             </div>
           </div>
@@ -42,6 +47,26 @@ export default function HeaderBanner(props: { sponsers: Sponsers[] }) {
       </section>
    
       <SponsersBanner sponsers={props.sponsers} />
+
+      <h2 style={{ marginTop: "1em" }}>
+        <strong className="">Our Partner Institutions</strong>
+      </h2>
+      <Row>
+        {props.partners.map((partners) => (
+          <Col
+            key={partners.id}
+            className="center-hv"
+            style={{ margin: "1.5em 0" }}
+            sm={3}
+          >
+            <img
+              src={partners.path}
+              style={{ width: partners.width }}
+              alt={partners.title}
+            />
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 }
