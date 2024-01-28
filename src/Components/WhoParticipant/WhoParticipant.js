c64ac360b4323dc0a6f62194f1afd5133c495c8c
Santosharawn7 <santosharawn7@gmail.com>
2024-01-28 01:52:19 -0500

    font review

--- src/Components/WhoParticipant/WhoParticipant.js
+++ src/Components/WhoParticipant/WhoParticipant.js
@@ -6,7 +6,7 @@ export default function WhoCanParticipate() {
       <h2 >
         <strong>Who Can Participate</strong>
       </h2>
-      <p> All interested students are invited to join, provided that they belong to any of the following groups below:</p>
+      <p className="pb-3 text-lg font-gotham"> All interested students are invited to join, provided that they belong to any of the following groups below:</p>
       <ul role="list" class="pb-3 text-lg font-gotham marker:text-my-red list-disc pl-5 md:px-10 space-y-3 text-black md:text-justify">
                 <li> Seneca Polytechnic students and/or graduates. </li>
                 <li> Students from one of our partner institutions - York University, Universidad del Rosario (Colombia), Panyapiwat Institute of Management (Thailand), World Citi Colleges (Philippines).</li>
