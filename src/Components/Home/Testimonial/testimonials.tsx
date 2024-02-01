0c1db6ccc4940aa3a28f2739bfbca0e4e7c4de6a
Santosharawn7 <santosharawn7@gmail.com>
2024-01-31 21:45:13 -0500

    sides fixed

--- src/Components/Home/Testimonial/testimonials.tsx
+++ src/Components/Home/Testimonial/testimonials.tsx
@@ -29,7 +29,7 @@ export default function Testimonials({
   };
 
   return (
-    <div className="mx-5">
+    <div className="page-content">
       <h2>
         <strong>Testimonials</strong>
       </h2>
