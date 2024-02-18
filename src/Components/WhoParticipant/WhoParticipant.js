b6b3f7a3d141cd4ce56a78795db6dceed911a66b
Santosharawn7 <santosharawn7@gmail.com>
2024-02-18 17:45:07 -0500

    logossss

--- src/Components/WhoParticipant/WhoParticipant.js
+++ src/Components/WhoParticipant/WhoParticipant.js
@@ -11,7 +11,7 @@ export default function WhoCanParticipate() {
       <p className="pb-3 text-lg font-gotham"> All interested students are invited to join, provided that they belong to any of the following groups below:</p>
       <ul role="list" class="pb-3 text-lg font-gotham marker:text-my-red list-disc pl-5 md:px-10 space-y-3 text-black md:text-justify">
                 <li> Seneca Polytechnic students and/or graduates. </li>
-                <li> Students from one of our partner institutions - York University, Universidad del Rosario (Colombia), Panyapiwat Institute of Management (Thailand), World Citi Colleges (Philippines).</li>
+                <li> Students from the partner institutions form their own team without Seneca students or alumni in the team - York University, Universidad del Rosario (Colombia), Panyapiwat Institute of Management (Thailand), World Citi Colleges (Philippines).</li>
                 <li> Full-time students enrolled in other post-secondary institutions grouped with a Seneca Polytechnic student or graduate.</li>
               </ul>
       <div >
