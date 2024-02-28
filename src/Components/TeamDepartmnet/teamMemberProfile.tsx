a31e4e978e8bfdf7b38ae07dcc41ed8faf3c81b7
Yanice <yyoyosze@gmail.com>
2024-02-28 00:50:47 -0500

    Team Members Updated

--- src/Components/TeamDepartmnet/teamMemberProfile.tsx
+++ src/Components/TeamDepartmnet/teamMemberProfile.tsx
@@ -7,20 +7,31 @@ export default function TeamMemberProfile({ member }: { member: Member }) {
   return (
     <div className="profile">
       <Row>
-        <Col sm={12}>
-          <img className="w-24 h-24 mx-auto mb-6 rounded-full" src={SampleImage} alt=""></img>
-          <div className="flex flex-wrap -mx-4">
-        <div className="w-full md:w-full lg:w-full px-4 mb-10">
-          <div className="h-full py-8 px-10 bg-my-red rounded-md text-center">
-            <h3 className="mb-2 text-xl md:text-2xl leading-tight font-gotham  font-semibold">{member.firstName} {member.lastName}</h3>
-            <span className="inline-block mb-2 text-medium font-medium font-gotham text-white">{member.designation}</span>            <div className="flex items-center justify-center">
-              <a className="hover:opacity-80" href={member.linkedinLink} target="_blank">
-              <img className="w-full h-full" src="https://logos-world.net/wp-content/uploads/2020/04/Linkedin-Logo-2003-2011.png" alt=""></img>
-              </a>
+      <Col sm={12}>
+      <div className="grid md:grid-cols-1">
+      <div className="container mt-12 mb-24 mx-auto">
+          <div className="mb-24 md:mb-0">
+            <div className="md:w-full lg:w-full rounded-lg shadow-lg h-full block bg-my-red">
+              <div className="flex justify-center">
+                <div className="flex justify-center" style={{"margin": "-75px"}}>
+                  <img 
+                    src={member.image} 
+                    className="rounded-full w-[300px] mx-auto border-2 border-solid border-my-red shadow-lg" 
+                    alt="" 
+                    style={{"width": "150px; "}} />
+                </div>
+              </div>
+              <div className="p-6">
+                <h5 className="text-lg text-white font-bold mb-4 mt-16">{member.firstName} {member.lastName}</h5>
+                <p className="text-white mb-6">{member.designation}</p>  
+                  <a href={member.linkedinLink} target="_blank">
+                    <img className="" src="https://logos-world.net/wp-content/uploads/2020/04/Linkedin-Logo-2003-2011.png" alt="" style={{"width":"100px; height: 24px"}}/>
+                  </a>
+              </div>
+             </div>
             </div>
           </div>
         </div>
-      </div>
         </Col>
       </Row>
     </div>
