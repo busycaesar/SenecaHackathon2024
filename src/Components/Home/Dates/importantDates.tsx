7abcbee10a70e137b2e58c32d4bfcadf1cd69a79
Nonthachai Plodthong <nonthachaiplodthong@Nonthachais-MacBook-Pro.local>
2024-03-02 16:50:28 -0500

    Fix UI Important Dates

--- src/Components/Home/Dates/importantDates.tsx
+++ src/Components/Home/Dates/importantDates.tsx
@@ -8,42 +8,37 @@ export default function ImportantDates(props: { dates: any }) {
     return new Date(a.eventDate).getTime() - new Date(b.eventDate).getTime();
   });
   return (
-    <>
-    <div className="page-content">
-
-      <h2>
-        <strong>Important Dates (2024)</strong>
-      </h2>
-      <Row>
-        {eventDates.map((date: any) => {
-          let eventDateWhole: Date = new Date(date.eventDate),
-            eventMonth: string = eventDateWhole.toLocaleString("en-US", {
-              month: "long",
-            }),
-            eventDate: string = eventDateWhole.getUTCDate().toString();
-            return (
-              <Col md={2} index={date._id}>
-              <DateBox
-                eventDate={`${eventMonth} ${eventDate}`}
-                eventTitle={date.eventTitle}
-                eventDescription={date.eventDescription}
-                />
-            </Col>
-          );
-        })}
-      </Row>
-      <div className="button pt-4 text-center md:mx-auto">
-                  
-                  <a
-                    href={`/roadmap`}
-                    rel="noreferrer"
-                  >
-                    <button className="font-bold ">
-                      Check Our Roadmap 
-                    </button>
-                  </a>
-                </div>
+    <section className="py-20 lg:py-40 bg-gradient-to-r from-slate-900 to-slate-700">
+      <div className="container px-4 mx-auto">
+        <div className="text-center">
+          <h2 className="text-6xl sm:text-7xl xl:text-8xl font-heading font-semibold text-white mb-10">
+            Important Dates (2024)
+          </h2>
+          <Row>
+            {eventDates.map((date: any) => {
+              let eventDateWhole: Date = new Date(date.eventDate),
+                eventMonth: string = eventDateWhole.toLocaleString("en-US", {
+                  month: "long",
+                }),
+                eventDate: string = eventDateWhole.getUTCDate().toString();
+              return (
+                <Col md={2} index={date._id}>
+                  <DateBox
+                    eventDate={`${eventMonth} ${eventDate}`}
+                    eventTitle={date.eventTitle}
+                    eventDescription={date.eventDescription}
+                  />
+                </Col>
+              );
+            })}
+          </Row>
+          <div className="button pt-4 text-center md:mx-auto">
+            <a href={`/roadmap`} rel="noreferrer">
+              <button className="font-bold ">Check Our Roadmap</button>
+            </a>
+          </div>
         </div>
-    </>
+      </div>
+    </section>
   );
 }
