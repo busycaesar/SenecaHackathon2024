9a8ff729fa0f0c6445455969029c06d8917f731a
Dev Shah <busycaesar@gmail.com>
2024-03-09 21:18:18 -0500

    Refactor(news.css): Update css for the news image

--- src/Components/News/newsThumbnail.tsx
+++ src/Components/News/newsThumbnail.tsx
@@ -4,14 +4,14 @@ import "./news.css";
 
 export default function NewsThumbnail({ news }: { news: News }) {
   return (
-    <div id="thumbnailImage" className="summarize">
-      <img src={news.photo} alt="Sample" />
-      <a href={`/news/${news._id}`} className="no-underline">
+    <a href={`/news/${news._id}`} className="no-underline">
+      <div id="thumbnailImage" className="summarize">
+        <img src={news.photo} alt="Sample" />
         <span className="text-my-red md:text-xl text-lg font-gotham">
           {news.title}
         </span>
-      </a>
-      <p className="text-[0.8rem]">{news.body}</p>
-    </div>
+        <p className="text-[0.8rem]">{news.body}</p>
+      </div>
+    </a>
   );
 }
